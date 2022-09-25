facc7dc371f845f74ab34cf8b23ab2f593dac6bd
xXG4briel <gabrielgeorge95@gmail.com>
2022-09-25 16:26:44 -0300

    novos carros

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -7,30 +7,13 @@ import { Container, Spacer } from './styles';
 
 const Page: React.FC = () => {
 
-  const images = [
-    // '3.894 accessories-m.avif',
-    'model-s.avif',
-    'model-y.avif',
-    'model-3.avif',
-    'model-x.avif',
-    'solar-panels.avif',
-    'solar-roof.avif',
-    'accessories.avif'
-    // '6.640 model-3-m.avif',
-    // '16.276 model-s-m.avif',
-    // '15.559 model-x-m.avif',
-    // '34.413 model-y-m.avif',
-    // '18.367 solar-panels-m.avif',
-    // '21.744 solar-roof-m.avif',
-  ]
   const models = [
-    {name: 'Model S', image: 'model-s'},
-    {name: 'Model Y', image: 'model-y'},
-    {name: 'Model 3', image: 'model-3'},
-    {name: 'Model X', image: 'model-x'},
-    {name: 'Solar Panels', image: 'solar-panels'},
-    {name: 'Solar Roof', image: 'solar-roof'},
-    {name: 'Accessories', image: 'accessories'},
+    {name: 'Corvette', image: 'corvette'},
+    {name: 'Mustang', image: 'mustang'},
+    {name: 'Audi R8', image: 'audi-r8'},
+    {name: 'Ferrari 458', image: 'ferrari-458'},
+    {name: 'Lamborghini Aventador', image: 'lamborghini'},
+    {name: 'Camaro', image: 'camaro'}
   ];
 
   return (
